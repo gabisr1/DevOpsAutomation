b4d0a24185ee6fd78059708457feb409bea579e6
Gabriel Sorici <youremail@example.com>
2025-02-04 19:25:27 +0200

    Added interactive calendar feature

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const path = require('path');
 
+const app = express();
+const port = 7000;
+
+// Setăm view engine pentru EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Configurăm un director pentru fișiere statice
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principală
 app.get('/', (req, res) => {
-    res.send('Hello, DevOps Automation!');
+    res.render('calendar');
 });
 
+// Pornim serverul
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
 });
